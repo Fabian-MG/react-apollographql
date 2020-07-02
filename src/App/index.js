df8e0f8dc57843c0e9ded83942666443438d8458
Fabian-MG <mirandafabian1510@gmail.com>
2020-07-02 17:43:05 -0500

    React-apollo configuration

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Profile from '../Profile';
+
+import './style.css'
+
+class App extends Component {
+  render() {
+    return <Profile />;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HttpLink } from "apollo-link-http";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import * as serviceWorker from "./serviceWorker";
+
+import App from "./App/index";
+
+import "./style.css";
+
+const GITHUB_BASE_URL = "https://api.github.com/graphql";
+const cache = new InMemoryCache();
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer bc9a75f0724633724d13d4c9773e7e2c5f9d5358`,
+  },
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
